e66afee14e96690b3990c7efb1a9a11fbec25995
Alex <fox228.af@gmail.com>
2021-04-27 16:51:03 +0300

    27.04.21

--- Game1_Hand.xaml.cs
+++ Game1_Hand.xaml.cs
@@ -20,10 +20,38 @@ namespace Arts_Project
     /// </summary>
     public partial class Game1_Hand : Page
     {
+        private List<Image> elements;
+        private Image dragdrop_img; 
         public Game1_Hand()
         {
             InitializeComponent();
-            
+            elements = new List<Image>
+            {
+                soil_img,
+                wind_img,
+                fire_img,
+                water_img,
+                ingot_img
+            };
+        }
+
+        private void finger_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            dragdrop_img = (Image)sender;
+            DragDrop.DoDragDrop(dragdrop_img, dragdrop_img.Source, DragDropEffects.Move);
+        }
+
+        private void Image_Drop(object sender, DragEventArgs e)
+        {
+            Image element = (from img in elements
+                            where img == sender
+                            select img).First();
+            element.Source = dragdrop_img.Source;
+        }
+
+        private void Image_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Move;
         }
     }
 }
